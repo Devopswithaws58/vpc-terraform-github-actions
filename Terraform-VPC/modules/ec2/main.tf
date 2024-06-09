ebf8e7a32fff7bd59a8086c817def857518ac5a3
Devopswithaws58 <devopswithaws58@gmail.com>
2024-06-09 23:54:18 +0530

    modifing terraform and github action files for automation

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -4,13 +4,13 @@ resource "aws_instance" "dev_docker_server" {
   ami                         = data.aws_ami.amazon-2.id
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [var.dev_SG_id]
-  key_name = "lwplabskey"
+  key_name                    = "lwplabskey"
   associate_public_ip_address = true
   subnet_id                   = var.dev_public_subnets_ids[count.index]
   availability_zone           = data.aws_availability_zones.available.names[count.index]
   user_data                   = local.name
   tags = {
-    "Name" = title("${var.project_name}-${terraform.workspace}-dev-docker-engine-${count.index + 1}")
+    "Name" = title("${var.project_name}-${terraform.workspace}-docker-engine-${count.index + 1}")
   }
 }
 locals {
